21072d8cb7bad2104c55fc7df635d4944b8d5b46
Charles <cweng@stuy.edu>
2017-12-09 19:07:35 -0500

    done

--- intro.js
+++ intro.js
@@ -4,35 +4,47 @@
 // HW 15-- Sequential Progress
 // 2017-12-11
 
+// the things you need from the orignal html
 var list = document.getElementById("thelist");
-
-// adding an element to the list
-var counter = 8
-const add = function(){
-  console.log(list.innerHTML);
-  var el = document.createElement("li");
-  el.innerHTML = "item " + counter;
-  counter++;
-  list.appendChild(el);
-}
-
 const b = document.getElementById("b");
-b.addEventListener("click", add);
-
-
-const over = function(){
-
-}
-const out = function(){
-
-}
-const click = function(){
-
-}
-
 const h = document.getElementById("h");
 var l = document.getElementsByTagName("li");
+const head = h.innerHTML;
 
-// l.addEventListener("mouseover", over);
-// l.addEventListener("mouseout", out);
-// l.addEventListener("click", click);
+
+// functions that do the things (li functions)
+const over = function(){
+  h.innerHTML = this.innerHTML;
+}
+const out = function(){
+  // console.log(this.innerHTML);
+  h.innerHTML = head;
+}
+const click = function(){
+  this.remove();
+}
+
+// add the events to an element
+function addE(n){
+  n.addEventListener("mouseover", over);
+  n.addEventListener("mouseout", out);
+  n.addEventListener("click", click);
+}
+
+// add the events to each list element
+for(var i = 0; i < l.length; i++)
+  addE(l[i]);
+
+// adding an element to the list (button functions)
+var counter = 8
+const add = function(){
+  // console.log(list.innerHTML);
+  var el = document.createElement("li");
+  el.innerHTML = "item " + counter;
+  // new lists also need the events
+  addE(el);
+  list.appendChild(el);
+  counter++;
+  // console.log(l);
+}
+b.addEventListener("click", add);
